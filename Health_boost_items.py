bd84b4bcaf6d1f75f5de8d1d4d32781b087cc5f3
Dean Jones <84123704+deanhsjones@users.noreply.github.com>
2021-05-13 13:09:30 +0200

    Create Health_boost_items.py

---
+++ Health_boost_items.py
@@ -0,0 +1,17 @@
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.get("stamina grains") and available_items.pop("stamina grains")
+
+print(health_points)
+print(available_items)
+
+health_points += available_items.get("power stew", 0) and available_items.pop("power stew")
+
+print(health_points)
+print(available_items)
+
+health_points += available_items.get("mystic bread", 0) and available_items.pop("mystic bread", 0) 
+
+print(health_points)
+print(available_items)
